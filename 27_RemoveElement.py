cacbc21b31aaebe07f1bda3942368ba9ab8ad27d
Aaditya-git <aadityab134@gmail.com>
2022-05-18 19:28:00 +0530

    Refactored folder strucutre

--- 27_RemoveElement.py
+++
@@ -1,8 +0,0 @@
-arr = list(map(int,input().split()))
-value = int(input())
-
-for i in range(0,len(arr)):
-    if value in arr:
-        print('value is :-',value)
-        arr.remove(value)
-print(arr)
